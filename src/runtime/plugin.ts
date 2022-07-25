7f73c7089dc904f9f6a96b2a38a932b4c3103122
Nathan Chase <zampa_@hotmail.com>
2022-07-25 13:14:42 -0400

    comment out plugins that aren't exporting default

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import advancedFormat from 'dayjs/plugin/advancedFormat'
-import weekday from 'dayjs/plugin/weekday'
+// import advancedFormat from 'dayjs/plugin/advancedFormat'
+// import weekday from 'dayjs/plugin/weekday'
 import { defineNuxtPlugin } from '#app'
 
 declare module '#app' {
