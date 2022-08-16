f7458f0bc4d36a9935c278fff96f3d81b2e26569
Nathan Chase <zampa_@hotmail.com>
2022-08-16 18:51:58 -0400

    remove unneeded #app interface

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -2,12 +2,6 @@ import { defineNuxtPlugin } from '#app'
 import dayjs from 'dayjs/esm/index.js'
 import '#build/dayjs.config.mjs'
 
-declare module '#app' {
-  interface NuxtApp {
-    $dayjs: dayjs.Dayjs
- }
-}
-
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $dayjs(date?: dayjs.ConfigType): dayjs.Dayjs
