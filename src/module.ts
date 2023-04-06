cad0962b59cb522f051e58323f212858b3c437f5
harmzeinstra <harm@nullix.nl>
2023-04-06 15:55:57 +0200

    * added node types for module path resolving

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,5 @@
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineNuxtModule, addPlugin, addTemplate } from '@nuxt/kit'
 import { generateConfigContents } from './gen'
 
